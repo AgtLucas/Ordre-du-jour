a0650eb95160aea3493c5b9c2394952d618f990b
Lucas <metallucas.ds@gmail.com>
2013-03-02 13:17:28 -0300

    Teste..

---
+++ config/db-sample.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Credentials to connect the DB.
+ * Rename this file to db.php and configure the user and password bellow.
+ */
+$config = array(
+	'DB_USER' 		=> 'user',
+	'DB_PASSWORD'	=> 'password'
+);
